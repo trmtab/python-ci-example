cc9c676ca5bc3ed0b53543c4a4a28196e4dab8d5
Chris Conlin <cconlin@protonmail.com>
2022-05-19 15:38:15 -0500

    Add auto-formatting workflow step and test

--- example/main.py
+++ example/main.py
@@ -1,11 +1,9 @@
 def example_method(*args, **kwargs):
-    pass
-
+    print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
 class ExampleClass:
     """
     This is an example class
     """
-
     def __init__(self, *args, **kwargs):
         pass
