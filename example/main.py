e36d557702203b90e93e4356c2feb13608819f1c
Chris Conlin <trmtab@users.noreply.github.com>
2022-05-19 22:35:39 +0000

    Trigger formatting

--- example/main.py
+++ example/main.py
@@ -1,9 +1,14 @@
-def example_method(*args, **kwargs):
-    print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+def example_method(
+        *args, **kwargs):
+    print(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    )
+
 
 class ExampleClass:
     """
     This is an example class
     """
+
     def __init__(self, *args, **kwargs):
         pass
