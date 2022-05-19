f1a4e3f3036e56bd41992459102ae3a04f43b303
Chris Conlin <trmtab@users.noreply.github.com>
2022-05-19 22:38:13 +0000

    :fixup: Auto-format code with psf/black

--- example/main.py
+++ example/main.py
@@ -1,5 +1,7 @@
 def example_method(
-        *args, **kwargs):
+
+        *args, 
+        **kwargs):
     print(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     )
